4c6a26d4faa97808a4639106c0ea90d7d0521383
Kevin Baik <mphist@gmail.com>
2020-02-16 15:55:20 -0500

    todo reOrder fix

--- src/containers/TodoListContainer.tsx
+++ src/containers/TodoListContainer.tsx
@@ -71,6 +71,9 @@ const TodoListContainer = () => {
     const result = Array.from(list);
     const [removed] = result.splice(startIndex, 1);
     result.splice(endIndex, 0, removed);
+    const temp = result[endIndex].order_id;
+    result[endIndex].order_id = result[startIndex].order_id;
+    result[startIndex].order_id = temp;
     return result;
   };
 
